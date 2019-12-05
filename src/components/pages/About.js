6d5826fc24b988795e4471f09a642031fd5b8dd3
conorbeamish <conorbeamish@hotmail.co.uk>
2019-12-05 16:39:05 +0000

    Add routes for about

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>A Todo list app to manage your tasks</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
