c802cd630c76f64ca2e125fe23375096d1224fe5
conorbeamish <conorbeamish@hotmail.co.uk>
2019-12-05 15:07:34 +0000

    Configure todos and Todoitem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from "./components/Todos";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Todo App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Job 1",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Job 2",
+        completed: true
+      },
+      {
+        id: 4,
+        title: "Job 3",
+        completed: false
+      }
+    ]
+  }
+
+  markComplete = (id) => {
+    console.log(id)
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <Todos  todos={this.state.todos}
+                markComplete={this.markComplete}
+        />
+      </div>
+    );
+  } 
 }
 
 export default App;
