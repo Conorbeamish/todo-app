7dda6a47dd1b60da9e44c5575ed7ec855bf3fe91
conorbeamish <conorbeamish@hotmail.co.uk>
2019-12-05 15:44:35 +0000

    Add style for Todo input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import Header from "./components/layout/Header";
 import './App.css';
 
 class App extends Component {
@@ -41,10 +43,14 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-        <Todos  todos={this.state.todos}
-                markComplete={this.markComplete}
-                delTodo = {this.delTodo}
-        />
+        <div className="container">
+          <Header/>
+          <AddTodo/>
+          <Todos  todos={this.state.todos}
+                  markComplete={this.markComplete}
+                  delTodo = {this.delTodo}
+          />
+        </div>
       </div>
     );
   } 
